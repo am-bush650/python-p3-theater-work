70a3df0cbd84fb298b97b69f88de1ab6d83ed067
felix mwangi <fmwauramwangi@gmail.com>
2025-03-11 03:17:55 +0300

    lib/models.py

--- lib/models.py
+++ lib/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
+from sqlalchemy import ForeignKey, Column, Integer, String, MetaData, Create_engine
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -8,3 +8,23 @@ convention = {
 metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
+
+
+class Audition(Base):
+    ___tablename___ = 'auditions'
+    actor = Column(String)
+    location = Column(String)
+    Phone = Column(Integer)
+    hired = Column(Boolean)
+    role_id = Column(Integer, primary_key=True)
+
+
+class Role(Base):
+    __tablename__ = 'roles'
+    character_name = Column(String)
+
+
+
+    """|| Column | Type |
+| --- | --- |
+| character_name | string |"""
