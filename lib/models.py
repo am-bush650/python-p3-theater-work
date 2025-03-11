2c5ada0c8c8c566a7dd8b58abb0956565d9585a7
felix mwangi <fmwauramwangi@gmail.com>
2025-03-11 03:52:17 +0300

    lib/models.py

--- lib/models.py
+++ lib/models.py
@@ -1,6 +1,7 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData, Create_engine
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy import ForeignKey, Column, Integer, String, Boolean, MetaData
+from sqlalchemy.orm import relationship, backref, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
 
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -12,19 +13,24 @@ Base = declarative_base(metadata=metadata)
 
 class Audition(Base):
     ___tablename___ = 'auditions'
+    id = Column(Integer, primary_key=True)
     actor = Column(String)
     location = Column(String)
     Phone = Column(Integer)
-    hired = Column(Boolean)
-    role_id = Column(Integer, primary_key=True)
+    hired = Column(Boolean, default=False)
+    role_id = Column(Integer, ForeignKey('roles.id'))
+
+    def call_back(self):
+        self.hired = True
 
 
 class Role(Base):
     __tablename__ = 'roles'
+    id = Column(Integer, primary_key=True)
     character_name = Column(String)
 
 
-
-    """|| Column | Type |
-| --- | --- |
-| character_name | string |"""
+engine = create_engine('sqlite:///theater.db')
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
