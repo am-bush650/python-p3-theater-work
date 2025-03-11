6cf9355deeaa42c312999747149fadfc5a3963fb
felix mwangi <fmwauramwangi@gmail.com>
2025-03-11 06:33:59 +0300

    alembic.ini

--- lib/models.py
+++ lib/models.py
@@ -1,7 +1,5 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, Boolean, MetaData
-from sqlalchemy.orm import relationship, backref, sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
+from sqlalchemy import ForeignKey, Column, Integer, String, Boolean, MetaData, create_engine
+from sqlalchemy.orm import relationship, backref, sessionmaker, declarative_base
 
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -12,7 +10,7 @@ Base = declarative_base(metadata=metadata)
 
 
 class Audition(Base):
-    ___tablename___ = 'auditions'
+    __tablename__ = 'auditions'
     id = Column(Integer, primary_key=True)
     actor = Column(String)
     location = Column(String)
@@ -23,11 +21,31 @@ class Audition(Base):
     def call_back(self):
         self.hired = True
 
+    role = relationship('Role', backref=backref('auditions'))
+
 
 class Role(Base):
     __tablename__ = 'roles'
     id = Column(Integer, primary_key=True)
     character_name = Column(String)
+
+    def lead(self):
+        hired_auditions = [audition for audition in self.auditions if audition.hired]
+        return hired_auditions[0] if hired_auditions else 'no actor has been hired for this role'
+
+    @property
+    def actors(self):
+        return[audition.actor for audition in self.auditions]
+    
+    @property
+    def locations(self):
+        return [audition.location for audition in self.auditions]
+    
+    def understudy(self):
+        hired_auditions = [audition for audition in self.auditions if audition.hired]
+        return hired_auditions[1] if len(hired_auditions) > 1 else 'no actor has been hired for understudy for this role'
+ 
+
 
 
 engine = create_engine('sqlite:///theater.db')
