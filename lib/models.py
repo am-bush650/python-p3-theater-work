647d445e209fd4a8be4387495db7c8f2b99de601
felix mwangi <fmwauramwangi@gmail.com>
2025-03-13 02:35:38 +0300

    lib/models.py

--- lib/models.py
+++ lib/models.py
@@ -11,27 +11,36 @@ Base = declarative_base(metadata=metadata)
 
 class Audition(Base):
     __tablename__ = 'auditions'
+
     id = Column(Integer, primary_key=True)
-    actor = Column(String)
-    location = Column(String)
-    Phone = Column(Integer)
+    actor = Column(String, nullable=False)
+    location = Column(String, nullable=False)
+    phone = Column(String, nullable=False)
     hired = Column(Boolean, default=False)
-    role_id = Column(Integer, ForeignKey('roles.id'))
+    role_id = Column(Integer, ForeignKey('roles.id', nullable=False))
+
+    role = relationship('Role', back_populates='auditions')
+    
 
     def call_back(self):
         self.hired = True
 
-    role = relationship('Role', backref=backref('auditions'))
+
+    def __repr__(self):
+        return f"<Audition(id={self.id}, actor='{self.actor}', hired={self.hired})>"
 
 
 class Role(Base):
     __tablename__ = 'roles'
+
     id = Column(Integer, primary_key=True)
-    character_name = Column(String)
+    character_name = Column(String, nullable=False)
+
+    auditions = relationship('Audition', back_populates='role', cascade="all, delete-orphan")
 
     def lead(self):
         hired_auditions = [audition for audition in self.auditions if audition.hired]
-        return hired_auditions[0] if hired_auditions else 'no actor has been hired for this role'
+        return hired_auditions[0] if hired_auditions else "no actor has been hired for this role"
 
     @property
     def actors(self):
@@ -39,16 +48,19 @@ class Role(Base):
     
     @property
     def locations(self):
-        return [audition.location for audition in self.auditions]
+        return[audition.location for audition in self.auditions]
     
     def understudy(self):
         hired_auditions = [audition for audition in self.auditions if audition.hired]
-        return hired_auditions[1] if len(hired_auditions) > 1 else 'no actor has been hired for understudy for this role'
+        return hired_auditions[1] if len(hired_auditions) > 1 else "no actor has been hired for understudy for this role"
  
 
+    def __repr__(self):
+        return f"<Role(id={self.id}, character_name='{self.character_name}')>"
+    
 
-
-engine = create_engine('sqlite:///theater.db')
+engine = create_engine('sqlite:///theater.db', echo=True)
 Base.metadata.create_all(engine)
+
 Session = sessionmaker(bind=engine)
 session = Session()
