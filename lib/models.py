1e68c069fe70ae114bb756ce8551c172f4e0e807
felix mwangi <fmwauramwangi@gmail.com>
2025-03-13 10:22:56 +0300

    lib/models.py

--- lib/models.py
+++ lib/models.py
@@ -19,7 +19,7 @@ class Audition(Base):
     hired = Column(Boolean, default=False)
     role_id = Column(Integer, ForeignKey('roles.id'))
 
-    role = relationship('Role', backref=backref('auditions')'auditions'\)
+    role = relationship('Role', backref=backref('auditions'))
     
 
     def call_back(self):
